11de19e0261b80ab413d3ebb42b7e81041e39ebc
No Nema <44880953+mrdahom4@users.noreply.github.com>
2019-07-26 10:12:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,15 +26,28 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "simo");
-    logChannel.send(`Invited by: <@${inviter.id}>`);
-  });
+client.on('ready', function(){
+console.log(`Logged in as ${client.user.tag}!`);    
+   var large = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور الكبيرة
+ var small = ['483055660209012736','480169573530861578','483055655800930315'];  // صور الصغيرة
+    setInterval(function (){  
+    client.user.setPresence({
+  status: 'dnd',
+ game: {
+    type: 1,
+    name: 'Mr LoRans ',
+     url: 'https://www.twitch.tv/n3k4a',
+     state: `new stream code `,
+    application_id: '532298006951624745',  
+     assets: {
+        small_text: ' Hi ' ,
+        large_text: `I'm LoRans :)` },
+    large_image:  `${large[Math.floor(Math.random() * large.length)]}`,
+    small_image:   `${small[Math.floor(Math.random() * small.length)]}`,
+    }
+ 
+    });
+    }, 5000);
 });
 
 client.login(process.env.BOT_TOKEN);
